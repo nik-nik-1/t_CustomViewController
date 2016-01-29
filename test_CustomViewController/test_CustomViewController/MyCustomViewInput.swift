dd998dfd01c08b84d715877aeb42fa7173bd8b96
nik <dementievnikolay@gmail.com>
2016-01-29 14:09:02 +0200

    new item

--- test_CustomViewController/test_CustomViewController/MyCustomViewInput.swift
+++ test_CustomViewController/test_CustomViewController/MyCustomViewInput.swift
@@ -25,4 +25,51 @@ import UIKit
   }
  
   
+  var view:UIView!
+  
+  /*@IBInspectable
+  var mytitleLabelText: String? {
+    get {
+      return titleLabel.text
+    }
+    set(mytitleLabelText) {
+      titleLabel.text = mytitleLabelText
+    }
+  }
+  
+  @IBInspectable
+  var myCustomImage:UIImage? {
+    get {
+      return myImage.image
+    }
+    set(myCustomImage) {
+      myImage.image = myCustomImage
+    }
+  }
+  */
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setup()
+  }
+  
+  required init(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)!
+    setup()
+  }
+  
+  func setup() {
+    view = loadViewFromNib()
+    view.frame = bounds
+    //view.autoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeightaddSubview(view)
+    view.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
+  }
+  
+  func loadViewFromNib() -> UIView {
+    let bundle  = NSBundle(forClass:self.dynamicType)
+    let nib     = UINib(nibName: "MyCustomViewInput", bundle: bundle)
+    let view    = nib.instantiateWithOwner(self, options: nil)[0] as! UIView
+    
+    return view
+  }
+  
 }
