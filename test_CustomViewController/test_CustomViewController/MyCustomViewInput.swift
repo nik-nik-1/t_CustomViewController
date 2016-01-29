7b5a30961416815485e46f92ab7b57f0142e90ce
nik <dementievnikolay@gmail.com>
2016-01-29 12:25:14 +0200

    convention rules

---
+++ test_CustomViewController/test_CustomViewController/MyCustomViewInput.swift
@@ -0,0 +1,28 @@
+//
+//  myCustomView_input.swift
+//  test_CustomViewController
+//
+//  Created by Admin on 28.01.16.
+//  Copyright © 2016 Admin. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class MyCustomViewInput: UIView {
+  
+  /*
+  // Only override drawRect: if you perform custom drawing.
+  // An empty implementation adversely affects performance during animation.
+  override func drawRect(rect: CGRect) {
+  // Drawing code
+  }
+  */
+  
+ 
+  @IBOutlet var myCustomInput: UITextField!
+  
+  @IBAction func myCustomInputOKButton(sender: AnyObject) {
+  }
+ 
+  
+}
