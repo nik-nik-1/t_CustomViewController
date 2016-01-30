3bc9097c148d1e1f3a4fae06b25e520853b05c4f
nik <dementievnikolay@gmail.com>
2016-01-30 19:36:14 +0200

    + add  searchController

--- test_CustomViewController/test_CustomViewController/ViewController.swift
+++ test_CustomViewController/test_CustomViewController/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
   @IBOutlet var MyTableView: UITableView!
   let swiftBlogs = TableViewManager().getMassForTableView()
   
+  let searchController = UISearchController(searchResultsController: nil)
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
